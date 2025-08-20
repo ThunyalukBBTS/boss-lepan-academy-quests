ed051d51160f8c697251f225520eae40ed2f9d79
thunyalukBBTS <thunyaluk.sa@kkumail.com>
2025-08-20 11:31:12 +0700

    feat: create brag document page and edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
+  get "/brag" => "brag#index", as: :brag
   root "quests#index"
 end
